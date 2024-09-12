b819915e91ebeb05a7bf0be304224b051ffd8ae7
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2024-09-12 11:28:08 +0200

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,27 @@
 using TriangleSingularity
 using Test
 
-
-@testset "TriangleSingularity.jl" begin
-    @test TriangleSingularity.hello_world() == "Hello, World!"
+begin
+    const mπ = 0.13957018
+    const mK = 0.493677
+    const mB = 5.279
+    const mKst = 0.89166
+    const mQ = 2mπ-0.01
+    #
+    const mK² = mK^2
+    const mπ² = mπ^2
+    const mB² = mB^2
+    const mKst² = mKst^2
+    const mQ² = mQ^2
+    #
+    const iϵ = 1e-10im
 end
 
+
+@testset "Basic chech" begin
+    M3², M2², M1² = mB², mKst², mQ²
+    m2², m3² = mπ², mπ²
+    m1² = mK²
+    #
+    @test triangleloop(m1², m2², m3², M1²-iϵ, M2², M3²) isa Complex{Float64}
+end
